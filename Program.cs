bdf777094259bd87abdfa3d160a385c82f55021e
Juan Carlos <jcmgx2006@gmail.com>
2026-06-16 00:54:28 -0500

    Base para cambiar libros

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // ricardo aqui va create y read
+    }
+
+    // Esto es el update es para cambiar de libro - precio -codigo y cantidad 
+    static void CambiarLibro(string[] codigos, string[] libros, double[] precios, int[] cantidades, int total)
+    {
+        string codigoBuscado;
+        int posicion = -1;
+
+        Console.WriteLine("CAMBIAR LIBRO");
+    }
+}
